14552410a39dacb05be94bbc1a313478a958d0b2
rohan257 <rohan.singh@bobble.ai>
2022-06-21 21:29:31 +0530

    Added VideoRepository Setup for the Project

---
+++ app/src/main/java/com/example/myvideoplayer/data/VideoRepository.kt
@@ -0,0 +1,39 @@
+package com.example.myvideoplayer.data
+
+import android.content.Context
+import com.example.myvideoplayer.data.local.VideoDao
+import com.example.myvideoplayer.data.model.Video
+import com.example.myvideoplayer.utils.readFile
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class VideoRepository @Inject constructor(
+    private val videoDao: VideoDao,
+    private val moshi: Moshi,
+    private val context: Context
+) {
+
+
+    fun getAllVideosList(): Flow<List<Video>> {
+        val listType = Types.newParameterizedType(List::class.java, Video::class.java)
+        val adapter: JsonAdapter<List<Video>> = moshi.adapter(listType)
+
+        val jsonString = context.assets.readFile("data.json")
+
+        return flowOf(adapter.fromJson(jsonString) ?: listOf())
+    }
+
+
+    fun getHistoryVideos() : Flow<List<Video>> = videoDao.getHistoryVideos()
+
+    suspend fun setVideoCount(video: Video) = videoDao.setViewCount(video.viewCount, video.id)
+
+    suspend fun setVideoTimeStamp(video: Video) = videoDao.setSeekTime(video.seekTime, video.id)
+
+    suspend fun setLastViewedTime(video: Video) = videoDao.setLastViewedTime(video.lastViewedTime, video.id)
+
+}
